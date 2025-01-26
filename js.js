d00770e9e50f60fa5bf553cb7ea7b2bc27dce27f
charl11e <146877892+charl11e@users.noreply.github.com>
2025-01-26 19:09:01 +0000

    Styled navbar

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 // Load navbar
 async function loadNavbar() {
     try {
+
+        // Emphasise the current page
         const navbar = document.getElementById("navbar-placeholder");
         const response = await fetch("/navbar.html");
         const navbarHTML = await response.text();
@@ -13,8 +15,48 @@ async function loadNavbar() {
                 navLinks[i].classList.add("active");
             };
         };
+
+        // Load user's saved theme if applicable
+        const theme = localStorage.getItem("theme");
+        if (theme) {
+            setTheme(theme);
+        } else {
+            setTheme("auto");
+        };
+
+        // Switch to light mode
+        document.getElementById("light-toggle").addEventListener("click", function() {
+            setTheme("light-mode");
+        });
+
+        // Switch to dark mode
+        document.getElementById("dark-toggle").addEventListener("click", function() {
+            setTheme("dark-mode");
+        });
+
+        // Switch to auto mode
+        document.getElementById("auto-toggle").addEventListener("click", function() {
+            setTheme("auto-mode");
+        });
+
     } catch(e) {
         console.error("Error loading navbar: ", e);
-    }
+    };
 };
 loadNavbar();
+
+// Theme switcher
+function setTheme(theme) {
+    document.body.classList.remove("light-mode", "dark-mode");
+    document.body.classList.add(theme);
+    localStorage.setItem("theme", theme);
+
+    document.getElementById("light-toggle").classList.remove("active");
+    document.getElementById("dark-toggle").classList.remove("active");
+    document.getElementById("auto-toggle").classList.remove("active");
+    document.getElementById(theme.split("-")[0] + "-toggle").classList.add("active");
+
+    if (theme == "auto-mode") {
+        console.log("need to implement");
+    }
+}
